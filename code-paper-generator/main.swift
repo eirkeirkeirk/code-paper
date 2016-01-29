1d178bc27464eb22ed8eb434044b4a2ff63365e3
Erik P. Hansen <eph@erikphansen.com>
2016-01-29 07:35:30 -0800

    Initial Commit

---
+++ code-paper-generator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  code-paper-generator
+//
+//  Created by Erik P. Hansen on 1/29/16.
+//  Copyright © 2016 Erik P. Hansen. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
